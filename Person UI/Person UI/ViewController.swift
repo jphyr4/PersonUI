2c589df7156880880c09ce26bf7656b26fb530f4
jphyr4 <35503354+jphyr4@users.noreply.github.com>
2018-06-22 16:10:29 -0500

    PersonUI Challenge

---
+++ Person UI/Person UI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by Jacob Paul Hassler on 6/21/18.
+//  Copyright © 2018 jphyr4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
